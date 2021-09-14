b42dccb06e19bc5cfc633fa4baa1a7e0c4f0bee8
Aaron Foucheaux <afoucheaux@gmail.com>
2021-09-14 14:49:38 -0500

    Added react-icons for like button

--- src/components/PlanetCard/PlanetCard.tsx
+++ src/components/PlanetCard/PlanetCard.tsx
@@ -1,13 +1,27 @@
-import React, {useState} from 'react';
+import  {useState} from 'react';
 import './PlanetCard.css';
 import {PlanetInfo} from '../../interface';
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai'
 
-const PlanetCard: React.FC<PlanetInfo> = ({title, date, explanation, url}): JSX.Element => {
+const PlanetCard = ({title, date, explanation, url}: PlanetInfo): JSX.Element => {
   const [liked, setLiked] = useState(false);
+
+  const useInput = () => {
+    if (liked) {
+      setLiked(false);
+    } else {
+      setLiked(true);
+    }
+  }
 
   return (
     <article>
-      <h1>{title}</h1>
+      <h1 className='title'>{title}</h1>
+      <img src={url} alt={`picture of ${title}`} className='planetImgage'/>
+      <p className='moreInfo'>{explanation}</p>
+      <p>{date}</p>
+      {liked && <AiFillHeart onClick={() => useInput()}></AiFillHeart>}
+      {!liked && <AiOutlineHeart onClick={() => useInput()}></AiOutlineHeart>}
     </article>
   )
 }
