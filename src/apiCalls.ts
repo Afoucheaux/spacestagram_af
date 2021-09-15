bfd4aa99bfc9fbf6954fb3edfe3246087b0742e3
Aaron Foucheaux <afoucheaux@gmail.com>
2021-09-15 15:52:06 -0500

    Added more images to api call

--- src/apiCalls.ts
+++ src/apiCalls.ts
@@ -2,7 +2,7 @@ const apodURL = 'https://api.nasa.gov/planetary/apod?api_key=';
 const key = 'RWiJWoTIhhkmJsg6Wejaa7n2Pdsxe0MmQaqhhLJY'
 
 export async function getPlanetaryDataJSON() {
-  const response = await fetch(`${apodURL}${key}&count=5`);
+  const response = await fetch(`${apodURL}${key}&count=10`);
   const planetaryData = await response.json();
   return planetaryData;
 }
