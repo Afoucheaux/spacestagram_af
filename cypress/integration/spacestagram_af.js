cdfd4330593b5512e604a545c4dd9511282d7e01
Aaron Foucheaux <afoucheaux@gmail.com>
2021-09-17 08:24:45 -0500

    Stubed api call - stubGetPlanetaryDataJSON bug fixed.

--- cypress/integration/spacestagram_af.js
+++ cypress/integration/spacestagram_af.js
@@ -1,4 +1,8 @@
 context('Spacestagram_af', () => {
 
-  
+  it('Should have a header on the main page, with the site title displayed on interval', () => {
+    cy.stubGetPlanetaryDataJSON()
+    cy.visit('http://localhost:3000/')
+  })
+
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,4 +1,4 @@
-Cypress.Commands.add('stubGetPlanetaryDataJSON()', () => {
-  cy.intercept('https://api.nasa.gov/planetary/apod?api_key=RWiJWoTIhhkmJsg6Wejaa7n2Pdsxe0MmQaqhhLJY',
+Cypress.Commands.add('stubGetPlanetaryDataJSON', () => {
+  cy.intercept('https://api.nasa.gov/planetary/apod?api_key=RWiJWoTIhhkmJsg6Wejaa7n2Pdsxe0MmQaqhhLJY&count=10',
    {fixture: 'testApiCallDataStub.js'})
 })
